cacc022ed9cceb81299ef2a72b800675101ab64b
raresmanea3333 <rares.manea26@gmail.com>
2019-10-31 12:43:02 +0200

    Created core patient list

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,14 +1,22 @@
-sap.ui.define([ 'sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel' ], function(Controller, JSONModel) {
-	'use strict';
+sap.ui.define(['sap/ui/core/mvc/Controller', 'sap/ui/model/json/JSONModel', "com/cerner/interns/SAPUI5_Demo/model/models"],
+	function (Controller, JSONModel, models) {
+		'use strict';
 
-	return Controller.extend('com.cerner.interns.SAPUI5_Demo.controller.View1', {
-		_data: {
-			dtValue: new Date()
-		},
+		return Controller.extend('com.cerner.interns.SAPUI5_Demo.controller.View1', {
+			_data: {
+				dtValue: new Date()
+			},
 
-		onInit: function(evt) {
-			var oModel = new JSONModel(this._data);
-			this.getView().setModel(oModel, 'systemDate');
-		}
+
+			onInit: function (evt) {
+				var oModel = new JSONModel(this._data);
+				this.getView().setModel(oModel, 'systemDate');
+
+
+				let pacientModel = models.createPacientModel();
+				this.getView().setModel(pacientModel, "patientList");
+
+				patientList.addStyleClass("myPatientList");
+			}
+		});
 	});
-});
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -10,6 +10,57 @@ sap.ui.define([
 			var oModel = new JSONModel(Device);
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
+		},
+
+		createPacientModel: function () {
+			let oData =
+			{
+				"Patients": [
+					{
+						"id": 1,
+						"name": "Mihai"
+					},
+
+					{
+						"id": 2,
+						"name": "Cornel"
+					},
+
+					{
+						"id": 3,
+						"name": "MARIAN"
+					},
+
+					{
+						"id": 4,
+						"name": "Ceaser"
+					},
+
+					{
+						"id": 5,
+						"name": "Relian"
+					},
+
+					{
+						"id": 6,
+						"name": "Reliana"
+					},
+
+					{
+						"id": 7,
+						"name": "Jonas"
+					},
+
+					{
+						"id": 8,
+						"name": "Rares"
+					}
+				]
+			};
+
+
+			let oModel = new JSONModel(oData);
+			return oModel;
 		}
 
 	};
