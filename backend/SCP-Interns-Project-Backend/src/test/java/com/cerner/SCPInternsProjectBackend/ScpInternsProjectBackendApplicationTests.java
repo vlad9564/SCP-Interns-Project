d05cd5189eac5cb25b19188e2dab49197c62407d
SandraTazlaoanu <sandra.tazlaoanu79@gmail.com>
2019-10-31 13:32:19 +0200

    Initial commit.

---
+++ backend/SCP-Interns-Project-Backend/src/test/java/com/cerner/SCPInternsProjectBackend/ScpInternsProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cerner.SCPInternsProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpInternsProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
