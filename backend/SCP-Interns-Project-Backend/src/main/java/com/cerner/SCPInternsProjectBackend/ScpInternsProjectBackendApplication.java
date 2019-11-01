07f322324b6a2b7ae768dc40702a69d633a498c3
dordea_dragos <dordeadragos@yahoo.com>
2019-11-01 14:29:15 +0200

    Impl GET request for all doctors; Replace JSON file to resources

--- backend/SCP-Interns-Project-Backend/src/main/java/com/cerner/SCPInternsProjectBackend/ScpInternsProjectBackendApplication.java
+++ backend/SCP-Interns-Project-Backend/src/main/java/com/cerner/SCPInternsProjectBackend/ScpInternsProjectBackendApplication.java
@@ -3,15 +3,11 @@ package com.cerner.SCPInternsProjectBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.cerner.SCPInternsProjectBackend.Util.DataGenerator;
-import com.cerner.SCPInternsProjectBackend.model.PatientDto;
-
 @SpringBootApplication
 public class ScpInternsProjectBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScpInternsProjectBackendApplication.class, args);
-		DataGenerator.generateDoctors();
 	}
 
 }
