159d46f46407f11f81bf5a8328fe44fd51f9ad34
andreibalau <andrei.balau@cerner.com>
2019-11-08 12:44:19 +0200

    refactoring updatePatient implementation

--- backend/SCP-Interns-Project-Backend/src/main/java/com/cerner/SCPInternsProjectBackend/service/PatientService.java
+++ backend/SCP-Interns-Project-Backend/src/main/java/com/cerner/SCPInternsProjectBackend/service/PatientService.java
@@ -1,6 +1,8 @@
 package com.cerner.SCPInternsProjectBackend.service;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -26,31 +28,26 @@ public class PatientService {
 	private File getPatientsFile() throws FileNotFoundException {
 		return ResourceUtils.getFile("classpath:patients.json");
 	}
-
-	private PatientDto findPatientById(String patientId) throws JsonParseException, JsonMappingException, IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		List<PatientDto> patients = mapper.readValue(getPatientsFile(), PatientsDto.class);
-		PatientDto patient = patients.stream().filter(value -> value.getId().equals(patientId)).findFirst().get();
-		return patient;
+	
+	private File getDoctorsFile() throws FileNotFoundException {
+		return ResourceUtils.getFile("classpath:doctors.json");
 	}
-
-	public PatientDto updatePatient(String patientId, DoctorDto body)
-			throws JsonParseException, JsonMappingException, IOException {
-
+	
+	private DoctorDto findDoctorById(String doctorId) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		DoctorsDto doctors = mapper.readValue(getDoctorsFile(), DoctorsDto.class);
+		DoctorDto doctor = doctors.stream().filter(value -> value.getId().equals(doctorId)).findFirst().get();
+		
+		return doctor;
+	}
+		
+	public PatientDto updatePatient(String patientId,String doctorId) throws IOException{
 		ObjectMapper mapper = new ObjectMapper();
 		PatientsDto patients = mapper.readValue(getPatientsFile(), PatientsDto.class);
-
-		PatientDto patient = this.findPatientById(patientId);
-		patient.setDoctor(body);
-		String fileName = "src/main/resources/patients.json";
-		File fisier = new File(fileName);
-		FileWriter fileWriter = new FileWriter(fisier);
-
-		patients.stream().filter(p -> p.getId().equals(patientId)).findFirst().get().setDoctor(body);
-
+		patients.stream().filter(p -> p.getId().equals(patientId)).findFirst().get().setDoctor(this.findDoctorById(doctorId));
+		FileWriter fileWriter = new FileWriter(new File("src/main/resources/patients.json"));
 		fileWriter.write(patients.toJSON());
-		fileWriter.flush();
 		fileWriter.close();
-		return patient;
+		return patients.stream().filter(value -> value.getId().equals(patientId)).findFirst().get(); 
 	}
 }
